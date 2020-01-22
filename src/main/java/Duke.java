5086c510dcd699ede41a2c0ad10cd2d000fb43e0
shunde_ <chiangshunde13@gmail.com>
2020-01-22 15:41:48 +0800

    finished Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,21 +1,10 @@
-import java.util.Scanner;
-
-public class Duke{
+public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        String effect = "************************************************************\n";
-        System.out.println(effect + "Hi! I'm\n" + logo);
-        System.out.println("What can I do for you?\n" + effect);
-        Scanner sc = new Scanner(System.in);
-        String command = sc.next();
-        while(!command.equals("bye")) {
-            System.out.println(effect + command + "\n" + effect);
-            command = sc.next();
-        }
-        System.out.println(effect + "Bye. Hope to see you again soon!\n" + effect);
+        System.out.println("Hello from\n" + logo);
     }
 }
