9abc02644e43065cf7c1f36d893c227ec6d8fd00
Chiang Shun De <chiangshunde13@gmail.com>
2020-01-22 16:32:45 +0800

    Finished Level 2
    Signed-off-by: shunde_ <chiangshunde13@gmail.com>

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke{
     public static void main(String[] args) {
@@ -7,16 +8,29 @@ public class Duke{
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
+        ArrayList<String> list = new ArrayList<>();
         System.out.println("Hi! I'm\n" + logo);
-        String effect = "************************************************************\n";
-        System.out.println(effect + "Hi! I'm\n" + logo);
+        String effect = "************************************************************";
+        System.out.println(effect + "\nHi! I'm\n" + logo);
         System.out.println("What can I do for you?\n" + effect);
         Scanner sc = new Scanner(System.in);
-        String command = sc.next();
+        String command = sc.nextLine();
         while(!command.equals("bye")) {
-            System.out.println(effect + command + "\n" + effect);
-            command = sc.next();
+            System.out.println(effect);
+            if (command.equals("list")) {
+                int index = 1;
+                for (String s : list) {
+                    System.out.println(index + ". " + s);
+                    index++;
+                }
+            }
+            else {
+                list.add(command);
+                System.out.println("added: " + command);
+            }
+            System.out.println(effect);
+            command = sc.nextLine();
         }
-        System.out.println(effect + "Bye! :) Hope to see you again soon!\n" + effect);
+        System.out.println(effect + "\n" + "Bye! :) Hope to see you again soon!\n" + effect);
     }
 }
