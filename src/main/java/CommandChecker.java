806311481c1ac79d6d7a1ace57aa14a55535f9e5
Chiang Shun De <chiangshunde13@gmail.com>
2020-01-24 02:02:25 +0800

    Finished Level 5
    Signed-off-by: shunde_ <chiangshunde13@gmail.com>

---
+++ src/main/java/CommandChecker.java
@@ -0,0 +1,53 @@
+import java.util.ArrayList;
+
+public class CommandChecker {
+
+    public void checkCommand(String command, ArrayList<Task> list) throws DukeException {
+        if (command.length() < 4) {
+            throw new DukeException("☹ OOPS!!! I'm sorry, your command is too short ;) \n");
+        } else if (command.contains("todo") || command.contains("event") || command.contains("deadline")) {
+            if (command.substring(0, 4).equals("todo")) {
+                if (command.equals("todo ") || command.equals("todo")) {
+                    throw new DukeException("☹ OOPS!!! The description of a todo cannot be empty.\n");
+                }
+            } else if (command.substring(0, 5).equals("event")) {
+                if ((command.equals("event")) || (command.equals("event "))) {
+                    throw new DukeException("☹ OOPS!!! The description of an event cannot be empty.\n");
+                }
+                if (!command.contains("/at")) {
+                    throw new DukeException("☹ OOPS!!! Did you forget to add /at for your event???\n");
+                }
+                if (command.split("/at")[1].length() < 2) {
+                    throw new DukeException("☹ OOPS!!! An event should have a day and time!!!\n");
+                }
+                if (!command.split("/at")[1].matches(".*\\d.*")) {
+                    throw new DukeException("☹ Did you forget to add a time to your event??? ☹ \n");
+                }
+            } else if (command.substring(0, 8).equals("deadline")) {
+                if ((command.equals("deadline ")) || (command.equals("deadline"))) {
+                    throw new DukeException("☹ OOPS!!! The description of a deadline cannot be empty.\n");
+                }
+                if (!command.contains("/by")) {
+                    throw new DukeException("☹ OOPS!!! Did you forget to add /by for your deadline???\n");
+                }
+            }
+        } else if (command.substring(0, 4).equals("done")) {
+            if (!command.contains(" ")) {
+                throw new DukeException("☹ OOPS!!! What task number have you accomplished?\n");
+            }
+            if (Integer.parseInt(command.substring(5)) > list.size()) {
+                throw new DukeException("☹ OOPS!!! The number you typed is too high!\n");
+            }
+            if (Integer.parseInt(command.substring(5)) < 0) {
+                throw new DukeException("☹ OOPS!!! You typed a negative number!\n");
+            }
+        } else {
+            if (command.contains("list") && !command.equals("list")) {
+                throw new DukeException("☹ OOPS!!! did you mean to type \"list\"? ☹ \n");
+            }
+            if (!command.substring(0, 4).equals("list")) {
+                throw new DukeException("☹ OOPS!!! I'm sorry, but I don't know what that means ☹ \n");
+            }
+        }
+    }
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 import java.util.ArrayList;
 
-public class Duke{
+public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -15,37 +15,49 @@ public class Duke{
         System.out.println("What can I do for you?\n" + effect);
         Scanner sc = new Scanner(System.in);
         String command = sc.nextLine();
-        while(!command.equals("bye")) {
-            System.out.println(effect);
-            if (command.equals("list")) {
-                System.out.println("Here are the tasks in your list:");
-                int index = 1;
-                for (Task t : list) {
-                    System.out.println(index + "." + t);
-                    index++;
-                }
+        System.out.println(command.split(" ")[0]);
+        while (!command.equals("bye")) {
+            CommandChecker cc = new CommandChecker();
+            try {
+                cc.checkCommand(command, list);
+                listAction(list, command, effect);
+            } catch (DukeException e) {
+                System.err.println(e);
             }
-            else if (command.split(" ")[0].equals("done")){
-                int target = Integer.parseInt(command.substring(5));
-                Task taskDone = list.get(target - 1);
-                taskDone.markDone();
-                System.out.println("Nice! I've marked this task as done:");
-                System.out.println(taskDone);
-            }
-            else {
-                if (command.startsWith("deadline")) {
-                    list.add(new Deadline(command.substring(9).split("/by")[0],command.substring(9).split("/by")[1]));
-                } else if (command.startsWith("todo")){
-                    list.add(new Todo(command.substring(5)));
-                } else if (command.startsWith("event")){
-                    list.add(new Event(command.substring(6).split("/at")[0], command.substring(6).split("/at")[1]));
-                }
-                System.out.println("Got it. I've added this task:\n" + list.get(list.size() - 1));
-                System.out.println("Now you have " + list.size() + " tasks in the list.");
-            }
-            System.out.println(effect);
             command = sc.nextLine();
         }
         System.out.println(effect + "\n" + "Bye! :) Hope to see you again soon!\n" + effect);
     }
+
+    public static ArrayList<Task> listAction(ArrayList<Task> list, String command, String effect) {
+        System.out.println(effect);
+        if (command.equals("list")) {
+            System.out.println("Here are the tasks in your list:");
+            int index = 1;
+            for (Task t : list) {
+                System.out.println(index + "." + t);
+                index++;
+            }
+        } else if (command.split(" ")[0].equals("done")) {
+            int target = Integer.parseInt(command.substring(5));
+            Task taskDone = list.get(target - 1);
+            taskDone.markDone();
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println(taskDone);
+        } else {
+            if (command.startsWith("deadline")) {
+                list.add(new Deadline(command.substring(9).split("/by")[0], command.substring(9).split("/by")[1]));
+            } else if (command.startsWith("todo")) {
+                list.add(new Todo(command.substring(5)));
+            } else if (command.startsWith("event")) {
+                list.add(new Event(command.substring(6).split("/at")[0], command.substring(6).split("/at")[1]));
+            }
+            System.out.println("Got it. I've added this task:\n" + list.get(list.size() - 1));
+            System.out.println("Now you have " + list.size() + " tasks in the list.");
+        }
+        System.out.println(effect);
+        return new ArrayList<>(list);
+    }
 }
+
+
